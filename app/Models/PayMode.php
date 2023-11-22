a2e5d4c6e9bead38c3566c75426e1a16166ac6c4
Sebas y Juli <jgomez4967@campuscecep.edu.co>
2023-11-22 16:41:20 -0500

    PAYMODE COMMIT

---
+++ app/Models/PayMode.php
@@ -0,0 +1,74 @@
+<?php
+
+class PayMode{
+        
+    private $id;
+    private $name;
+    private $observation;
+
+public function __construct($id, $name, $observation) {
+    $this ->id= $id;
+    $this ->name=$name;
+    $this ->observation=$observation;    
+}
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of observation
+     */ 
+    public function getObservation()
+    {
+        return $this->observation;
+    }
+
+    /**
+     * Set the value of observation
+     *
+     * @return  self
+     */ 
+    public function setObservation($observation)
+    {
+        $this->observation = $observation;
+
+        return $this;
+    }
+}
